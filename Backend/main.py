3e4913b50e593dfedca6fbd5d35e8a0c7190e4b3
Guimaraes <69005421+guimaraesdev0@users.noreply.github.com>
2024-07-22 15:21:19 -0300

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -82,4 +82,6 @@ def extract_tables():
     return send_file(csv_path, as_attachment=True, download_name=csv_filename)
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
+
